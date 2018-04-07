969a9539203411691e17f7413091121c71fe01d7
folukeOpenuni <folukeagbede@icloud.com>
2018-04-07 12:54:18 +0100

    link and html body
    added link to the main page

---
+++ index2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bana's Travel Blog</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="banaTravel.css">
+        <title></title>
+    </head>
+    <body>
+        <div id="banner">
+    <h1>Bana's Travel Blog</h1>
+  </div>
+  <div id="blog">
+    <div class="post">
+      <h2>Saturday Market</h2>
+      <h3>January 7th</h3>
+      <strong class="opening-line">On Saturday, Dilara took us to the market.</strong>
+      <p>It is a wonderfully eclectic mix of goods from all over the world - delightfully random. I dusted off my bargaining skills to buy a metal bracelet with camels on it, and Maggie bought a lapis lazuli necklace from Afghanistan. In the evening, we walked along Gulf Road towards the marina, passed through Marina Mall, and ate dinner at a wonderful little cafe that we will definitely be frequenting. Ive developed a slight obsession with lentil soul... maybe its because I miss Focos Indian lentil chili.</p>
+      <div class="image-container">
+        <img src="market.jpg" />
+      </div>
+      <p>We took a catamaran from the marina to Failaka Island, where we got to explore a bit, relax on the beach, and go swimming. The water in the Gulf is incredibly still; I havent seen a wave yet, and when you look out on the horizon it seems to go forever. Something about the color of the water and maybe the dust in the air make it seem hazier than the Atlantic coast. I have some cool pictures of the city skyline as we returned on the boat.</p>
+      <p>That night, I hung out in a cafe with two friends, drinking traditional lemonade with mint. Its delicious; maybe I should send in a flavor suggestion to Morano Gelato? Then we drove around the city for a while. Cruising around is a really valid way to spend the time when a tank of gas costs about seven bucks. </p>
+    </div>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
