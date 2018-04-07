d592b16f25b83b65b3de2fd4414b68cf5cc61880
folukeOpenuni <folukeagbede@icloud.com>
2018-04-07 12:50:38 +0100

    Bana's Travel
    link added to head section

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bana's Travel Blog</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="banaTravel.css">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
